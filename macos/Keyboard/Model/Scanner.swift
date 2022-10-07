f8867756e22a10257eca064934627643d1a24fc3
Jason Barrie Morley <jbmorley@mac.com>
2022-10-07 16:31:47 -0400

    refactor: Event handling out of the UI code

--- macos/Keyboard/Model/Scanner.swift
+++ macos/Keyboard/Model/Scanner.swift
@@ -5,6 +5,8 @@
 //  Created by Jason Barrie Morley on 03/10/2022.
 //
 
+import AppKit
+import Carbon
 import CoreBluetooth
 import Foundation
 
@@ -28,6 +30,63 @@ class Scanner: NSObject, ObservableObject, CBCentralManagerDelegate {
         case connected
         case disconnecting
     }
+
+    // Mapping table between macOS keycodes and TinyUSB_Mouse_and_Keyboard codes.
+    // https://github.com/cyborg5/TinyUSB_Mouse_and_Keyboard/blob/master/TinyUSB_Mouse_and_Keyboard.h
+    static let mapping: [Int: UInt8] = [
+
+        // Layout-independent keycodes.
+        kVK_Return: 0xB0,
+        kVK_Tab: 0xB3,
+//        kVK_Space:
+        kVK_Delete: 0xB2,
+        kVK_Escape: 0xB1,
+        kVK_Command: 0x83,
+        kVK_Shift: 0x81,
+        kVK_CapsLock: 0xC1,
+        kVK_Option: 0x82,
+        kVK_Control: 0x80,
+        kVK_RightCommand: 0x87,
+        kVK_RightShift: 0x85,
+        kVK_RightOption: 0x86,
+        kVK_RightControl: 0x84,
+//        kVK_Function:
+//        kVK_VolumeUp:
+//        kVK_VolumeDown:
+//        kVK_Mute:
+
+        kVK_F1: 0xC2,
+        kVK_F2: 0xC3,
+        kVK_F3: 0xC4,
+        kVK_F4: 0xC5,
+        kVK_F5: 0xC6,
+        kVK_F6: 0xC7,
+        kVK_F7: 0xC8,
+        kVK_F8: 0xC9,
+        kVK_F9: 0xCA,
+        kVK_F10: 0xCB,
+        kVK_F11: 0xCC,
+        kVK_F12: 0xCD,
+        kVK_F13: 0xF0,
+        kVK_F14: 0xF1,
+        kVK_F15: 0xF2,
+        kVK_F16: 0xF3,
+        kVK_F17: 0xF4,
+        kVK_F18: 0xF5,
+        kVK_F19: 0xF6,
+        kVK_F20: 0xF7,
+
+//        kVK_Help:
+        kVK_PageUp: 0xD3,
+        kVK_PageDown: 0xD6,
+        kVK_ForwardDelete: 0xD4,
+        kVK_Home: 0xD2,
+        kVK_End: 0xD5,
+        kVK_LeftArrow: 0xD8,
+        kVK_RightArrow: 0xD7,
+        kVK_DownArrow: 0xD9,
+        kVK_UpArrow: 0xDA,
+    ]
 
     @Published var state: State = .idle
     @Published var peripherals: Set<CBPeripheral> = []
@@ -96,6 +155,46 @@ class Scanner: NSObject, ObservableObject, CBCentralManagerDelegate {
         }
         state = .connecting
         centralManager.connect(peripheral, options: nil)
+    }
+
+    private func writeData(data: Data) {
+        guard let connection = connection else {
+            return
+        }
+        connection.peripheral.writeValue(data,
+                                         for: connection.txCharacteristic,
+                                         type: CBCharacteristicWriteType.withoutResponse)
+    }
+
+    func disableKeyboardInput() {
+        writeData(data: Data([MessageType.disable.rawValue, 0]))
+    }
+
+    func enableKeyboardInput() {
+        writeData(data: Data([MessageType.enable.rawValue, 0]))
+    }
+
+    func sendEvent(_ event: NSEvent) {
+
+        switch event.type {
+        case .keyDown:
+            if let keyCode = Self.mapping[Int(event.keyCode)] {
+                writeData(data: Data([MessageType.keyDown.rawValue, keyCode, 0]))
+            } else if let character = event.characters?.first,
+                      let characterCode = character.asciiValue {
+                writeData(data: Data([MessageType.keyDown.rawValue, characterCode, 0]))
+            }
+        case .keyUp:
+            if let keyCode = Self.mapping[Int(event.keyCode)] {
+                writeData(data: Data([MessageType.keyUp.rawValue, keyCode, 0]))
+            } else if let character = event.characters?.first,
+                      let characterCode = character.asciiValue {
+                writeData(data: Data([MessageType.keyUp.rawValue, characterCode, 0]))
+            }
+        default:
+            print("Unsupported event.")
+        }
+
     }
 
 }
@@ -178,7 +277,9 @@ extension Scanner: CBPeripheralDelegate {
     }
 
 
-    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+    func peripheral(_ peripheral: CBPeripheral,
+                    didUpdateNotificationStateFor characteristic: CBCharacteristic,
+                    error: Error?) {
         if (error != nil) {
             print("Error changing notification state:\(String(describing: error?.localizedDescription))")
 
@@ -189,25 +290,6 @@ extension Scanner: CBPeripheralDelegate {
         if (characteristic.isNotifying) {
             print ("Subscribed. Notification has begun for: \(characteristic.uuid)")
         }
-    }
-
-    // TODO: Move this stuff into the SerialConnection.
-
-    func writeData(data: Data) {
-        guard let connection = connection else {
-            return
-        }
-        connection.peripheral.writeValue(data,
-                                         for: connection.txCharacteristic,
-                                         type: CBCharacteristicWriteType.withoutResponse)
-    }
-
-    func disableKeyboardInput() {
-        writeData(data: Data([MessageType.disable.rawValue, 0]))
-    }
-
-    func enableKeyboardInput() {
-        writeData(data: Data([MessageType.enable.rawValue, 0]))
     }
 
 }
--- macos/Keyboard/Views/NSInputView.swift
+++ macos/Keyboard/Views/NSInputView.swift
@@ -6,69 +6,8 @@
 //
 
 import AppKit
-import Carbon
 
 class NSInputView: NSView {
-
-    // TODO: These should be unsigned
-    // TODO: Should this be an enum?
-
-    // Mapping table between macOS keycodes and TinyUSB_Mouse_and_Keyboard codes.
-    // https://github.com/cyborg5/TinyUSB_Mouse_and_Keyboard/blob/master/TinyUSB_Mouse_and_Keyboard.h
-    static let mapping: [Int: UInt8] = [
-
-        // Layout-independent keycodes.
-        kVK_Return: 0xB0,
-        kVK_Tab: 0xB3,
-//        kVK_Space:
-        kVK_Delete: 0xB2,
-        kVK_Escape: 0xB1,
-        kVK_Command: 0x83,
-        kVK_Shift: 0x81,
-        kVK_CapsLock: 0xC1,
-        kVK_Option: 0x82,
-        kVK_Control: 0x80,
-        kVK_RightCommand: 0x87,
-        kVK_RightShift: 0x85,
-        kVK_RightOption: 0x86,
-        kVK_RightControl: 0x84,
-//        kVK_Function:
-//        kVK_VolumeUp:
-//        kVK_VolumeDown:
-//        kVK_Mute:
-
-        kVK_F1: 0xC2,
-        kVK_F2: 0xC3,
-        kVK_F3: 0xC4,
-        kVK_F4: 0xC5,
-        kVK_F5: 0xC6,
-        kVK_F6: 0xC7,
-        kVK_F7: 0xC8,
-        kVK_F8: 0xC9,
-        kVK_F9: 0xCA,
-        kVK_F10: 0xCB,
-        kVK_F11: 0xCC,
-        kVK_F12: 0xCD,
-        kVK_F13: 0xF0,
-        kVK_F14: 0xF1,
-        kVK_F15: 0xF2,
-        kVK_F16: 0xF3,
-        kVK_F17: 0xF4,
-        kVK_F18: 0xF5,
-        kVK_F19: 0xF6,
-        kVK_F20: 0xF7,
-
-//        kVK_Help:
-        kVK_PageUp: 0xD3,
-        kVK_PageDown: 0xD6,
-        kVK_ForwardDelete: 0xD4,
-        kVK_Home: 0xD2,
-        kVK_End: 0xD5,
-        kVK_LeftArrow: 0xD8,
-        kVK_RightArrow: 0xD7,
-        kVK_DownArrow: 0xD9,
-        kVK_UpArrow: 0xDA,
-    ]
 
     var scanner = Scanner()
 
@@ -97,22 +36,12 @@ class NSInputView: NSView {
     }
 
     override func keyDown(with event: NSEvent) {
-        if let keyCode = Self.mapping[Int(event.keyCode)] {
-            scanner.writeData(data: Data([MessageType.keyDown.rawValue, keyCode, 0]))
-        } else if let character = event.characters?.first,
-                  let characterCode = character.asciiValue {
-            scanner.writeData(data: Data([MessageType.keyDown.rawValue, characterCode, 0]))
-        }
+        scanner.sendEvent(event)
         super.keyDown(with: event)
     }
 
     override func keyUp(with event: NSEvent) {
-        if let keyCode = Self.mapping[Int(event.keyCode)] {
-            scanner.writeData(data: Data([MessageType.keyUp.rawValue, keyCode, 0]))
-        } else if let character = event.characters?.first,
-                  let characterCode = character.asciiValue {
-            scanner.writeData(data: Data([MessageType.keyUp.rawValue, characterCode, 0]))
-        }
+        scanner.sendEvent(event)
         super.keyUp(with: event)
     }
 
