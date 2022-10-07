5a80fabb6a1a4cd6477d6d8e6c37a31af7f40560
Jason Barrie Morley <jbmorley@mac.com>
2022-10-07 16:59:28 -0400

    fix: Only show connection options when connected

--- macos/Keyboard/Views/ContentView.swift
+++ macos/Keyboard/Views/ContentView.swift
@@ -8,62 +8,50 @@
 import CoreBluetooth
 import SwiftUI
 
-extension BluetoothManager.State {
-
-    var localizedDescription: String {
-        switch self {
-        case .idle:
-            return "Idle"
-        case .scanning:
-            return "Scanning..."
-        case .connecting:
-            return "Connecting..."
-        case .connected:
-            return "Connected"
-        case .disconnecting:
-            return "Disconnecting..."
-        }
-    }
-
-}
-
 struct ContentView: View {
 
     @ObservedObject var bluetoothManager = BluetoothManager()
 
     var body: some View {
+
         HStack {
-            List {
-                ForEach(bluetoothManager.peripherals) { peripheral in
-                    HStack {
-                        Text(peripheral.name ?? "Unknown")
-                        Spacer()
-                        Button("Connect") {
-                            bluetoothManager.connect(peripheral)
+            switch bluetoothManager.state {
+            case .idle:
+                Text("Idle")
+            case .scanning:
+                List {
+                    ForEach(bluetoothManager.peripherals) { peripheral in
+                        HStack {
+                            Text(peripheral.name ?? "Unknown")
+                            Spacer()
+                            Button("Connect") {
+                                bluetoothManager.connect(peripheral)
+                            }
                         }
                     }
                 }
-            }
-            .safeAreaInset(edge: .bottom) {
-                Text(bluetoothManager.state.localizedDescription)
-                    .padding()
-            }
-            VStack {
-                InputView(bluetoothManager: bluetoothManager)
-                HStack {
-                    Button("Disable Input") {
-                        bluetoothManager.disableKeyboardInput()
-                    }
-                    Button("Enable Input") {
-                        bluetoothManager.enableKeyboardInput();
-                    }
-                    Button("Disconnect") {
-                        bluetoothManager.disconnect()
+            case .connecting:
+                Text("Connecting...")
+            case .connected:
+                VStack {
+                    InputView(bluetoothManager: bluetoothManager)
+                    HStack {
+                        Button("Disable Input") {
+                            bluetoothManager.disableKeyboardInput()
+                        }
+                        Button("Enable Input") {
+                            bluetoothManager.enableKeyboardInput();
+                        }
+                        Button("Disconnect") {
+                            bluetoothManager.disconnect()
+                        }
                     }
                 }
+                .padding()
+            case .disconnecting:
+                Text("Disconnecting...")
             }
         }
-        .padding()
         .onAppear {
             bluetoothManager.start()
         }
