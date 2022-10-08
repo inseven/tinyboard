d3e021b778983d4f5d9c977a50b74295f5031a05
Jason Barrie Morley <jbmorley@mac.com>
2022-10-07 20:13:29 -0400

    refactor: Rename the tests to match the new project name

--- macos/InputStickTests/InputStickTests.swift
+++ macos/InputStickTests/InputStickTests.swift
@@ -19,27 +19,14 @@
 // SOFTWARE.
 
 import XCTest
+@testable import InputStick
 
-final class KeyboardUITestsLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
+final class InputStickTests: XCTestCase {
 
     override func setUpWithError() throws {
-        continueAfterFailure = false
     }
 
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
+    override func tearDownWithError() throws {
     }
+
 }
--- macos/InputStickUITests/InputStickUITestsLaunchTests.swift
+++ macos/InputStickUITests/InputStickUITestsLaunchTests.swift
@@ -19,31 +19,23 @@
 // SOFTWARE.
 
 import XCTest
-@testable import Keyboard
 
-final class KeyboardTests: XCTestCase {
+final class InputStickUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        continueAfterFailure = false
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
