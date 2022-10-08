a2ea63ddd51216402bd619813ebb215b4e0e8015
Jason Barrie Morley <jbmorley@mac.com>
2022-10-07 20:09:40 -0400

    docs: Add copyright and license to the source files

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,3 +1,23 @@
+// Copyright (c) 2022 InSeven Limited
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <bluefruit.h>
 #include <TinyUSB_Mouse_and_Keyboard.h>
 
@@ -144,7 +164,5 @@ void loop () {
     }
 
   }
-
-  // TODO: Is this really necessary?
-  // delay(10);
+  
 }
