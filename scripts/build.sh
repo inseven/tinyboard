2fb53bf06f2983b5cbc01cc51d023308377598b7
Jason Barrie Morley <jbmorley@mac.com>
2022-10-16 17:20:06 -0400

    Remove unused [not-Mac] App Store profile

--- scripts/build.sh
+++ scripts/build.sh
@@ -129,7 +129,6 @@ echo "$APPLE_DISTRIBUTION_CERTIFICATE_PASSWORD" | build-tools import-base64-cert
 echo "$MACOS_DEVELOPER_INSTALLER_CERTIFICATE_PASSWORD" | build-tools import-base64-certificate --password "$KEYCHAIN_PATH" "$MACOS_DEVELOPER_INSTALLER_CERTIFICATE_BASE64"
 
 # Install the provisioning profiles.
-build-tools install-provisioning-profile "TinyBoard_App_Store_Profile.mobileprovision"
 build-tools install-provisioning-profile "TinyBoard_Mac_App_Store_Profile.provisionprofile"
 
 # Build and archive the macOS project.
