1c5e4a3ff1b2829a77fb23eb0b3b4237089662da
Jason Barrie Morley <jbmorley@mac.com>
2022-10-17 22:34:14 -0400

    Move the Set extension into a separate file

--- macos/TinyBoard/Model/ApplicationModel.swift
+++ macos/TinyBoard/Model/ApplicationModel.swift
@@ -23,26 +23,6 @@ import SwiftUI
 
 import Diligence
 
-extension Set: RawRepresentable where Element: Codable {
-    public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8),
-              let result = try? JSONDecoder().decode([Element].self, from: data)
-        else {
-            return nil
-        }
-        self = Set(result)
-    }
-
-    public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self),
-              let result = String(data: data, encoding: .utf8)
-        else {
-            return "[]"
-        }
-        return result
-    }
-}
-
 class ApplicationModel: NSObject, ObservableObject {
 
     @Published var isEnabled = false;
