c021ce5185f6abceec06e116e9c4f57bb813e4d9
Jason Barrie Morley <jbmorley@mac.com>
2022-12-03 23:40:14 +0000

    feat: Show a HUD when toggling capture

--- macos/TinyBoard/Model/ApplicationModel.swift
+++ macos/TinyBoard/Model/ApplicationModel.swift
@@ -21,8 +21,86 @@
 import Carbon
 import Combine
 import SwiftUI
+import Cocoa
 
 import Diligence
+
+struct HudMaterial: NSViewRepresentable {
+
+    func makeNSView(context: Context) -> NSVisualEffectView {
+        let hudMaterial = NSVisualEffectView()
+        hudMaterial.blendingMode = .behindWindow
+        hudMaterial.isEmphasized = true
+        hudMaterial.material = .hudWindow
+        hudMaterial.state = NSVisualEffectView.State.active
+        return hudMaterial
+    }
+
+    func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
+
+    }
+}
+
+extension View where Self == HudMaterial {
+
+    static var hudMaterial: HudMaterial { HudMaterial() }
+
+}
+
+struct HUDView: View {
+
+    @Environment(\.closeWindow) var closeWindow
+    @State var window: NSWindow?
+
+    private struct LayoutMetrics {
+        static let cornerRadius = 20.0
+        static let size = 200.0
+        static let imageFontSize = 72.0
+    }
+
+    let isEnabled: Bool
+
+    @State var showHUD: Bool = false
+
+    var body: some View {
+        ZStack {
+            if showHUD {
+                VStack() {
+                    Spacer()
+                    Image(systemName: isEnabled ? "keyboard.fill" : "keyboard")
+                        .font(.system(size: LayoutMetrics.imageFontSize))
+                    Spacer()
+                    Text(isEnabled ? "Capture On" : "Capture Off")
+                        .font(.title)
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(.hudMaterial)
+                .cornerRadius(LayoutMetrics.cornerRadius)
+                .onDisappear {
+                    print("Closing window!")
+                    window?.close()
+                }
+            }
+        }
+        .frame(width: LayoutMetrics.size, height: LayoutMetrics.size)
+        .hookWindow { window in
+            self.window = window
+        }
+        .onAppear {
+            withAnimation {
+                print("Appeared!")
+                showHUD = true
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.4) {
+                withAnimation {
+                    showHUD = false
+                }
+            }
+        }
+    }
+}
+
 
 class ApplicationModel: NSObject, ObservableObject {
 
@@ -53,11 +131,20 @@ class ApplicationModel: NSObject, ObservableObject {
         }
     }()
 
+    // TODO: Main actor?
     override init() {
         super.init()
         eventTap.delegate = self
         eventTap.start()
         deviceManager.delegate = self
+
+        $isEnabled
+            .receive(on: DispatchQueue.main)
+            .sink { isEnabled in
+                print("Show HUD \(isEnabled)")
+                self.showHud(isEnabled: isEnabled)
+            }
+            .store(in: &cancellables)
     }
 
     func showAbout() {
@@ -67,6 +154,21 @@ class ApplicationModel: NSObject, ObservableObject {
             aboutWindow.center()
         }
         aboutWindow.makeKeyAndOrderFront(nil)
+    }
+
+    // See https://stackoverflow.com/questions/69845268/swiftui-is-it-possible-to-create-macos-huds-like-the-ones-that-comes-up-when-ch.
+    func showHud(isEnabled: Bool) {
+        dispatchPrecondition(condition: .onQueue(.main))
+        
+        let panel = NSPanel(contentViewController: NSHostingController(rootView: HUDView(isEnabled: isEnabled)))
+        panel.backgroundColor = .clear
+        panel.isMovable = false
+        panel.level = .floating
+        panel.hidesOnDeactivate = false
+        panel.isFloatingPanel = true
+        panel.styleMask = [.borderless, .hudWindow, .nonactivatingPanel]
+        panel.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
+        panel.orderFrontRegardless()
     }
 
     func trustDevice(_ device: Device) {
