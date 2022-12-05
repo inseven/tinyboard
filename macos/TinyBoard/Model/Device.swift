18f51adb7699a3f78e6361b048cebf261c610c55
Jason Morley <hello@jbmorley.co.uk>
2022-12-05 15:09:39 +0000

    fix: Crash with large mouse movements (#21)

--- macos/TinyBoard/Model/Device.swift
+++ macos/TinyBoard/Model/Device.swift
@@ -119,8 +119,8 @@ class Device: NSObject, ObservableObject, Identifiable {
     func sendEvent(_ event: NSEvent) {
 
         if event.type == .mouseMoved || event.type == .leftMouseDragged {
-            let deltaX = withUnsafeBytes(of: Int8(ceil(event.deltaX)).bigEndian, Array.init)[0]
-            let deltaY = withUnsafeBytes(of: Int8(ceil(event.deltaY)).bigEndian, Array.init)[0]
+            let deltaX = withUnsafeBytes(of: Int8(clamping: Int(ceil(event.deltaX))).bigEndian, Array.init)[0]
+            let deltaY = withUnsafeBytes(of: Int8(clamping: Int(ceil(event.deltaY))).bigEndian, Array.init)[0]
             writeData(data: Data([MessageType.mouseMove.rawValue, deltaX, deltaY]))
             return
         } else if event.type == .leftMouseDown {
