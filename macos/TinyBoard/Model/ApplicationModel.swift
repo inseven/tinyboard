b8d3b21a42d83dc3a05c14cf267c846bab53de48
Jason Morley <hello@jbmorley.co.uk>
2024-01-26 21:24:49 -1000

    fix: Thank Lukas Fittl 🙏🏻 (#57)

--- macos/TinyBoard/Model/ApplicationModel.swift
+++ macos/TinyBoard/Model/ApplicationModel.swift
@@ -45,6 +45,7 @@ class ApplicationModel: NSObject, ObservableObject {
                 Credit("Jason Morley", url: URL(string: "https://jbmorley.co.uk"))
             }
             Acknowledgements("Thanks") {
+                Credit("Lukas Fittl")
                 Credit("Michael Dales")
                 Credit("Pavlos Vinieratos")
                 Credit("Sarah Barbour")
