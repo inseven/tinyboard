78c51d57cef9faf4831b5644c679c5ec9362c627
Jason Morley <hello@jbmorley.co.uk>
2025-02-07 10:36:04 -1000

    docs: Update the copyright to 2025 (#64)

--- macos/TinyBoard/Model/ApplicationModel.swift
+++ macos/TinyBoard/Model/ApplicationModel.swift
@@ -1,4 +1,4 @@
-// Copyright (c) 2022-2024 Jason Morley
+// Copyright (c) 2022-2025 Jason Morley
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -38,7 +38,7 @@ class ApplicationModel: NSObject, ObservableObject {
     private var cancellables: Set<AnyCancellable> = []
 
     private lazy var aboutWindow: NSWindow = {
-        return NSWindow(repository: "inseven/tinyboard", copyright: "Copyright © 2022-2024 Jason Morley") {
+        return NSWindow(repository: "inseven/tinyboard", copyright: "Copyright © 2022-2025 Jason Morley") {
             Action("GitHub", url: URL(string: "https://github.com/inseven/tinyboard")!)
         } acknowledgements: {
             Acknowledgements("Developers") {
