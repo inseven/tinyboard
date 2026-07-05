2880da202a2c3ec74fde3e8a1a7019d211874d0c
Jason Morley <hello@jbmorley.co.uk>
2026-07-05 12:52:10 -1000

    feat: 📦 Publish standalone macOS (Developer ID) builds
    This change drops support for the Mac App Store in favor of Developer ID builds.

--- scripts/release.sh
+++ scripts/release.sh
@@ -24,24 +24,7 @@ set -e
 set -o pipefail
 set -x
 
-# This script expects the macOS PKG to be passed as the first argument, and any additional files to be attached to the
-# GitHub release to be passed as subsequent arguments.
-
-# Upload the macOS build.
-xcrun altool --upload-app \
-    -f "$1" \
-    --primary-bundle-id "uk.co.jbmorley.tinyboard.apps.appstore" \
-    --apiKey "$APPLE_API_KEY_ID" \
-    --apiIssuer "$APPLE_API_KEY_ISSUER_ID" \
-    --type macos
-
 # Actually make the release.
-FLAGS=()
-if $CHANGES_INITIAL_DEVELOPMENT ; then
-    FLAGS+=("--prerelease")
-elif $CHANGES_PRE_RELEASE ; then
-    FLAGS+=("--prerelease")
-fi
 gh release create "$CHANGES_TAG" --title "$CHANGES_QUALIFIED_TITLE" --notes-file "$CHANGES_NOTES_FILE" "${FLAGS[@]}"
 
 # Upload the attachments.
